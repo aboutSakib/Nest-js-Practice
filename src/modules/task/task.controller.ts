da767e845f254f29840cbfbd563932c01aab5b97
Sakib Ahmed <sakibhasan94470@gmail.com>
2025-03-03 11:26:17 +0600

    Use pagination

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TaskService } from './task.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
@@ -12,11 +21,15 @@ export class TaskController {
     return this.taskService.create(createTaskDto);
   }
 
-
-  
   @Get()
-  findAll() {
-    return this.taskService.findAll();
+  async findAll(
+    @Query('page') page: number,
+    @Query('perPage') perPage: number,
+  ) {
+    return await this.taskService.findAll(
+      page ? +page : 0,
+      perPage ? +perPage : 10,
+    );
   }
 
   @Get(':id')
--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -5,7 +5,7 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { Task } from './entities/task.entity';
 
-@Injectable()
+@Injectable() 
 export class TaskService {
   constructor(
     @InjectRepository(Task) private todoappRepository: Repository<Task>,
@@ -14,12 +14,24 @@ export class TaskService {
     const newTask = {
       task: createTaskDto.task,
       isCompleted: false,
-    };
+    }; 
     return this.todoappRepository.save(newTask);
   }
 
-  findAll() {
-    return this.todoappRepository.find();
+  async findAll(page, perPage) {
+    const skip = perPage * (page - 1);
+    const [tasks, total] = await this.todoappRepository.findAndCount({
+      select: ['id', 'task', 'createdAt'],
+      skip: skip,
+      take: perPage,
+    });
+    return {
+      data: tasks,
+      total: total,
+      CurrentPage: page,
+      perPage: perPage,
+      totalPage: Math.ceil(total / perPage),
+    };
   }
 
   findOne(id: number) {
